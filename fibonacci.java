54e206b8e47a0fa08fb929bc887568c7597d5c12
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2023-04-07 10:45:07 +0530

    Update fibonacci.java
    changed the variables

--- fibonacci.java
+++ fibonacci.java
@@ -1,15 +1,16 @@
+// adding a comment
 class FibonacciExample1{  
 public static void main(String args[])  
 {    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
+ int p1=0,p2=1,p3,i,count=10;    // changed the variables
+ System.out.print(p1+" "+p2);//printing 0 and 1    
     
  for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
  {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
+  p3=p1+p2;    
+  System.out.print(" "+p3);    
+  p1=p2;    
+  p2=p3;    
  }    
   
 }}  
