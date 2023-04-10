9d33867c47758d85afcf55c1ce2a4d389b2585c3
Ayush A Gupta <ayush.a.gupta@ukg.com>
2023-04-10 21:58:09 +0530

    Upadting insertion sort java file

--- insertion_sort.java
+++ insertion_sort.java
@@ -6,14 +6,14 @@ public class InsertionSort {
 		int n = arr.length;
 		for (int i = 1; i < n; ++i) {
 			int key = arr[i];
-			int j = i - 1;
+			int j = i + 1;
 
 			/* Move elements of arr[0..i-1], that are
 			greater than key, to one position ahead
 			of their current position */
 			while (j >= 0 && arr[j] > key) {
 				arr[j + 1] = arr[j];
-				j = j - 1;
+				j = j + 1;
 			}
 			arr[j + 1] = key;
 		}
